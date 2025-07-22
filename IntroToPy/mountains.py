cd3500ec11312d5aa76b51a0a228fc406c57c6ad
clumsy261 <rucareanueric@gmail.com>
2025-07-22 12:28:55 +0300

    Fixed mountains.py not taking special chars from the large sample

--- IntroToPy/mountains.py
+++ IntroToPy/mountains.py
@@ -5,7 +5,7 @@ munti = {}
 inalt = {}
 names = []
 count= 0
-with open("IntroToPy/mountains.tsv", "r") as data:
+with open("IntroToPy/mountains_db.tsv", "r",encoding= "UTF-8") as data:
     for line in data.readlines():
         part = line.split("\t", -1)
         #print(f"{part[0]} _ {part[1]} _ {part[2]} _ {part[3]}")
@@ -24,20 +24,24 @@ print(f"THe number of countries :: {len(tari.keys())}")
 print(f"The unmber of unknown altitudes :: {len(inalt["NULL"])}")
 sort_inalt = sorted(inalt.keys())
 sort_inalt.pop()
-sort_inalt = [int(x) for x in sort_inalt]
+sort_inalt = [round(float(x)) for x in sort_inalt]
 #print(f"{names[[inalt[smth]][0]]}")
 print(f"Minimum altitude :: {sort_inalt[0]}; Maximum random :: {sort_inalt[-1]} ")
 print(f"The average value of altitude :: {statistics.mean(sort_inalt)}")
 print(f"Median of the heights :: {statistics.median(sort_inalt)}")
 topp =int( input("How many mountains should be on the podium? \n"))
 lopp =0
-#print(f"{inalt[898]}")
+print(f"{names[791]}")
 if topp > len(sort_inalt):
     topp = len(sort_inalt)
     print(f"Hey! Those are too many mountains!")
     print(f"There are only {topp} mountains to show:")
 for n in range(1,int(topp)):
     place = sort_inalt[int(-n)]
+    if str(place) not in inalt:
+        print(place)
+        break
     for h in inalt[str(place)]:
-        print(f"The number {lopp+1} place goes to {names[h]}")
-        lopp+=1
+        if lopp < topp:
+            print(f"The number {lopp+1} place goes to {names[h]}")
+            lopp+=1
