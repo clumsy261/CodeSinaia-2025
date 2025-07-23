22ddb55ffcc27f1a8b674663b5f0d1c87fac0a6e
clumsy261 <rucareanueric@gmail.com>
2025-07-23 09:43:18 +0300

    Added and fixed the entire PyTDD directory

--- PyTDD/clumsy_converter.py
+++ PyTDD/clumsy_converter.py
@@ -1,10 +1,12 @@
+# new roman_converter.py but better
+
 def roman_converter (num):
     str=""
     value = [1000, 500, 100, 50, 10, 5, 1]
     letter = ["M", "D", "C", "L", "X", "V", "I"]
     poz=0
     if type(num) == type(11) :
-        if num in range (0,4000):
+        if num in range (1,4000):
             while poz < len(value):
                 while num >= value[poz] :
                     str += letter[poz]
@@ -12,3 +14,4 @@ def roman_converter (num):
                 poz += 1
             return str
     return None
+
